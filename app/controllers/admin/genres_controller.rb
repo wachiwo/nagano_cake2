8356cfab1b1f3e253d3d6455935ccd5035c493b1
EC2 Default User <ec2-user@ip-172-31-10-240.ec2.internal>
2022-03-20 07:13:52 +0000

    編集作成

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -12,14 +12,19 @@ class Admin::GenresController < ApplicationController
 
   def index
     @genre = Genre.new
-     #@genres = Genre.all
+    @genres = Genre.all
+    
   end
 
   def edit
     @genre = Genre.find(params[:id])
+
   end
 
   def update
+    genre = Genre.find(params[:id])
+    genre.update(genre_params)
+    redirect_to admin_genres_path(genre.id)
   end
 
   private # ストロングパラメータ
