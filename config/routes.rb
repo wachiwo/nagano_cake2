9ec2c0d122e2f77a9c37b24ae71b05fe6a40788d
EC2 Default User <ec2-user@ip-172-31-10-240.ec2.internal>
2022-03-21 18:07:52 +0000

    今日の作業終わり

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   
   namespace :public do
+    get 'items/index'
+    get 'items/show'
+  end
+  namespace :public do
    resources :customers, only: [:show, :edit, :update]
    patch '/users/:id/unsubscribe' => 'users#unsubscribe', as: 'unsubscribe'
   end
