d97bde33851a62699e6eb512abdd944ebe075656
EC2 Default User <ec2-user@ip-172-31-10-240.ec2.internal>
2022-03-22 18:46:58 +0000

    配送先一覧

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,5 +1,20 @@
 class Public::AddressesController < ApplicationController
+
+  def new
+    @address = Addresses.new
+   
+  end
+    def create
+      @address = Addresses.new
+    address = Address.new(address_params)
+    address.save
+
+    #redirect_to public_addresses_index_path
+  end
+
+
   def index
+    @addresses = Address.all
   end
 
   def edit
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_21_043420) do
+ActiveRecord::Schema.define(version: 2022_03_22_171947) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -38,6 +38,16 @@ ActiveRecord::Schema.define(version: 2022_03_21_043420) do
     t.integer "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "addresses", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "postal_code", null: false
+    t.string "address", null: false
+    t.integer "customer_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["customer_id"], name: "index_addresses_on_customer_id"
   end
 
   create_table "admins", force: :cascade do |t|
@@ -103,5 +113,6 @@ ActiveRecord::Schema.define(version: 2022_03_21_043420) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "addresses", "customers"
   add_foreign_key "items", "genres"
 end
