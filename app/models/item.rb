b2e2133bb53864a6a6117451daffb8d6ac4b7a74
EC2 Default User <ec2-user@ip-172-31-10-240.ec2.internal>
2022-03-23 17:20:19 +0000

    税込み価格表示

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,9 @@ class Item < ApplicationRecord
   belongs_to :genre, optional: true
   enum sales_status: { on_sale: 0, draft: 1 }
   has_many :cart_items, dependent: :destroy
+  
+  # 消費税を加えた商品価格
+    def add_tax_price
+        (self.price * 1.10).round
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  
+
   namespace :public do
-    
+
     resources:items, only:[:index, :show]
   end
 
@@ -23,7 +23,7 @@ Rails.application.routes.draw do
   namespace :admin do
     resources:customers, only: [:index, :show, :edit]
   end
-  
+
   namespace :admin do
     resources :items, only: [:new, :index, :create, :edit, :show, :update]
   end
