6fb01830fbebf09c9bf8f89aed4ccfee10295595
wachiwo <takumaro.d@gmail.com>
2022-04-04 18:12:55 +0000

    管理側作成

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,19 +1,14 @@
 class Admin::GenresController < ApplicationController
 
-  def new
-    @genre = Genre.new
-  end
-
-  def create
-    genre = Genre.new(genre_params)# ここでuninitialized constant Admin::GenresController::Genre エラーがでる
-    genre.save
-    redirect_to '/admin/genres'
-  end
-
-  def index
+    def index
     @genre = Genre.new
     @genres = Genre.all
 
+    end
+  def create
+    @genre = Genre.new(genre_params)
+    @genre.save!
+    redirect_to '/admin/genres'
   end
 
   def edit
@@ -24,7 +19,7 @@ class Admin::GenresController < ApplicationController
   def update
     genre = Genre.find(params[:id])
     genre.update(genre_params)
-    redirect_to admin_items_path(genre.id)
+    redirect_to admin_genres_path
   end
 
   private # ストロングパラメータ
